02389949aa8794a69717052167becddd1bfb04d0
Christian1100 <39745022+Christian1100@users.noreply.github.com>
2024-06-03 17:53:11 +0200

    Update http.py

--- gameguard_utils/http.py
+++ gameguard_utils/http.py
@@ -1,9 +1,26 @@
 import aiohttp
+from typing import Optional
 
 
-async def post(url: str, headers: dict):
+async def get(url: str, body: dict, headers: dict):
     async with aiohttp.ClientSession() as session:
         async with session.get(url, headers=headers) as response:
+            if response.status != 200:
+                return None
+            return await response.json()
+
+
+async def post(url: str, headers: dict, body: Optional[dict] = None):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url,  json=body, headers=headers) as response:
+            if response.status != 200:
+                return None
+            return await response.json()
+
+
+async def put(url: str, headers: dict, body: Optional[dict] = None):
+    async with aiohttp.ClientSession() as session:
+        async with session.put(url, json=body, headers=headers) as response:
             if response.status != 200:
                 return None
             return await response.json()
