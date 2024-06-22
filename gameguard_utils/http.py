edb3a923260fbe39cb1dbc8b84463a4f34497c7c
Christian1100 <39745022+Christian1100@users.noreply.github.com>
2024-06-23 00:18:42 +0200

    Update http.py

--- gameguard_utils/http.py
+++ gameguard_utils/http.py
@@ -10,17 +10,17 @@ async def get(url: str, headers: Optional[dict[str, str]] = None, params: Option
             return await response.json()
 
 
-async def post(url: str, headers: Optional[dict] = None, body: Optional[dict] = None):
+async def post(url: str, headers: Optional[dict] = None, params: Optional[dict[str, Any]] = None, body: Optional[dict] = None):
     async with aiohttp.ClientSession() as session:
-        async with session.post(url, json=body, headers=headers) as response:
+        async with session.post(url, json=body, headers=headers, params=params) as response:
             if response.status != 200:
                 return None
             return await response.json()
 
 
-async def put(url: str, headers: Optional[dict] = None, body: Optional[dict] = None):
+async def put(url: str, headers: Optional[dict] = None, params: Optional[dict[str, Any]] = None, body: Optional[dict] = None):
     async with aiohttp.ClientSession() as session:
-        async with session.put(url, json=body, headers=headers) as response:
+        async with session.put(url, json=body, headers=headers, params=params) as response:
             if response.status != 200:
                 return None
             return await response.json()
