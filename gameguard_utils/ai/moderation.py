26c9013e5e3b64d43fad8777143d95e5056736d8
Christian1100 <39745022+Christian1100@users.noreply.github.com>
2024-12-18 15:08:08 +0100

    Update moderation.py

--- gameguard_utils/ai/moderation.py
+++ gameguard_utils/ai/moderation.py
@@ -1,10 +1,11 @@
 from openai import AsyncClient
+from typing import List
 
 
 NSFW_THRESHOLD = 0.9
 
 
-async def violates_tos(client: AsyncClient, prompt: str, allow_nsfw: bool, threshold: float = 0.4) -> bool:
+async def violates_text_tos(client: AsyncClient, prompt: str, allow_nsfw: bool, threshold: float = 0.4) -> bool:
     response = await client.moderations.create(model="omni-moderation-latest", input=prompt)
     scores = response.results[0].category_scores
     if any(
@@ -26,3 +27,19 @@ async def violates_tos(client: AsyncClient, prompt: str, allow_nsfw: bool, thres
         return True
     
     return not allow_nsfw and scores.sexual > threshold
+
+
+async def violates_image_tos(client: AsyncClient, image_inputs: List[str], threshold: float = 0.4):
+    response = await client.moderations.create(
+        model="omni-moderation-latest",
+        input=[{"type": "image_url", "image_url": {"url": i}} for i in image_inputs]
+    )
+    scores = response.results[0].category_scores
+    return any([
+        scores.sexual > threshold,
+        scores.violence > threshold,
+        scores.violence_graphic > threshold,
+        scores.self_harm > threshold,
+        scores.self_harm_intent > threshold,
+        scores.self_harm_instructions > threshold,
+    ])
