b584d38713ac70b1e8a483c0826ecda5e5c1cb09
Christian1100 <39745022+Christian1100@users.noreply.github.com>
2024-12-20 01:53:31 +0100

    Update http.py

--- gameguard_utils/http.py
+++ gameguard_utils/http.py
@@ -1,5 +1,6 @@
 import aiohttp
-from typing import Optional, Any
+from fastapi import Request, HTTPException, status
+from typing import Optional, List, Any
 
 
 async def get(url: str, headers: Optional[dict[str, str]] = None, params: Optional[dict[str, Any]] = None):
@@ -25,6 +26,7 @@ async def put(url: str, headers: Optional[dict] = None, params: Optional[dict[st
                 return None
             return await response.json()
 
+
 async def delete(url: str, headers: Optional[dict] = None, params: Optional[dict[str, Any]] = None):
     async with aiohttp.ClientSession() as session:
         async with session.delete(url, headers=headers, params=params) as response:
@@ -32,3 +34,12 @@ async def delete(url: str, headers: Optional[dict] = None, params: Optional[dict
                 return None
             return await response.json()
             
+
+def validate_query_params(request: Request, required_params: List[str]):
+    query_params_set = set(request.query_params.keys())
+    for param in required_params:
+        if param not in query_params_set:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=f"Missing parameter: {param}"
+            )
